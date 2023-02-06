c1d0fe507184b21cfdbcf4fca16fd1fdb67549fe
lovesponge <guy.kellermann@gmail.com>
2023-02-06 18:10:42 +0200

    add orgmode and ufo folding

---
+++ lua/user/orgmode.lua
@@ -0,0 +1,47 @@
+local status_ok_ayucolors, ayucolors = pcall(require, "ayu.colors")
+if not status_ok_ayucolors then
+  return
+end
+local status_ok, orgmode = pcall(require, "orgmode")
+if not status_ok then
+  return
+end
+
+vim.o.conceallevel=2
+vim.o.concealcursor='nc'
+
+--[[ hi link OrgTodo special ]]
+
+orgmode.setup_ts_grammar()
+
+orgmode.setup {
+  org_agenda_files = { "~/Sync/orgtasks/Tasks.org" },
+  org_default_notes_file = "~/Sync/orgtasks/Tasks.org",
+  org_todo_keywords = { "TODO(t)", "|", "DONE(d)", "CANCELLED(c)" },
+  --[[ win_split_mode = { "float", 0.9 }, ]]
+  --[[ org_todo_keyword_faces = { ]]
+  --[[   TODO = ":foreground " .. ayucolors.accent ]]
+  --[[ }, ]]
+  org_hide_leading_stars = true,
+  org_tags_column = -20,
+  org_capture_templates = {
+    t = "T-Rex",
+    tt = {
+      description = "Planned",
+      template = "* TODO %? :@work:+trex:\n  SCHEDULED: <%<%Y-%m-%d %a>>"
+    },
+    tu = {
+      description = "Unplanned",
+      template = "* TODO %? :@work:+trex:unplanned:\n  SCHEDULED: <%<%Y-%m-%d %a>>"
+    },
+    e = "e-shot",
+    et = {
+      description = "Planned",
+      template = "* TODO %? :@work:+eshot:\n  SCHEDULED: <%<%Y-%m-%d %a>>"
+    },
+    weu = {
+      description = "Unplanned",
+      template = "* TODO %? :@work:+eshot:unplanned:\n  SCHEDULED: <%<%Y-%m-%d %a>>"
+    }
+  }
+}
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,19 +2,34 @@ local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
 	return
 end
+local status_ok_parsers, parsers = pcall(require, "nvim-treesitter.parsers")
+if not status_ok_parsers then
+	return
+end
+
+local parser_config = parsers.get_parser_configs()
+parser_config.org = {
+  install_info = {
+    url = 'https://github.com/milisims/tree-sitter-org',
+    revision = 'main',
+    files = { 'src/parser.c', 'src/scanner.cc' }
+  },
+  filetype = 'org'
+}
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = {
+  ensure_installed = "all", -- one of "all" or a list of languages
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "" }, -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = {
     enable = true,
     disable = { "" }
   },
+  --[[ additional_vim_regex_highlighting = {'org'} ]]
 })
